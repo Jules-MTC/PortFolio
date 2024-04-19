64c6d320b89d90916dd38bbeae28200a0e666e90
Tortue_Geniale <julesantoine@gmail.com>
2024-04-19 17:25:02 +0200

    update but work in progress

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
@@ -9,21 +11,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/send-email', (req, res) => {
-    console.log("HERE");
     const { name, email, phone, message } = req.body;
-
+// CHANGER TO A SIMPLE PASSWORD
     // Configuration de nodemailer pour envoyer l'e-mail
     const transporter = nodemailer.createTransport({
-        service: 'Gmail',
+        service: 'Outlook',
         auth: {
-            user: 'email',
-            pass: 'pwd'
+            user: process.env.EMAIL_FROM_ADDRESS,
+            pass: process.env.EMAIL_FROM_PASSWORD
         }
     });
 
     const mailOptions = {
-        from: email,
-        to: 'email',
+        from: process.env.EMAIL_FROM_ADDRESS,
+        to: process.env.EMAIL_TO_ADDRESS,
         subject: `Nouveau message de ${name}`,
         text: `Nom: ${name}\nEmail: ${email}\nTéléphone: ${phone}\nMessage: ${message}`
     };
@@ -41,5 +42,5 @@ app.post('/send-email', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Serveur démarré sur http://localhost:${port}`);
+    console.log(`Server start on http://localhost:${port}`);
 });
