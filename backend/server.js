eea5a063fca4a1f3da233f886570216980214c9c
Jules-MTC <julesantoine11@gmail.com>
2024-05-27 21:47:50 +0200

    wip

--- backend/server.js
+++ backend/server.js
@@ -6,28 +6,33 @@ const nodemailer = require("nodemailer");
 const packageJson = require("../package.json");
 const cors = require("cors");
 const i18n = require("i18n");
-const session = require('express-session');
-const secret = require('crypto').randomBytes(64).toString('hex');
-
+const session = require("express-session");
+const secret = require("crypto").randomBytes(64).toString("hex");
 const app = express();
 const port = 3000;
 const upload = multer();
 
 app.use(cors());
 app.use(upload.none());
+app.use(express.json());
 
 i18n.configure({
   locales: ["en", "fr"],
-  directory: __dirname + '/locales',
+  directory: __dirname + "/locales",
   defaultLocale: "en",
   objectNotation: true,
 });
 
-app.use(session({
-  secret: secret,
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(i18n.init);
+
+app.use(
+  session({
+    secret: secret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
 
 app.post("/send-email", (req, res) => {
   const { name, email, phone, message } = req.body;
@@ -69,23 +74,24 @@ app.get("/api/version", (req, res) => {
   res.json({ version: packageJson.version });
 });
 
-// Middleware pour définir la langue à partir de la session
 app.use((req, res, next) => {
-  if (req.session && req.session.language) {
-    i18n.setLocale(req.session.language);
-  }
+  const language =
+    req.headers["accept-language"] || req.session.language || "en";
+  i18n.setLocale(language);
   next();
 });
 
 app.post("/api/set-language", (req, res) => {
   const selectedLanguage = req.body.language;
+  if (!selectedLanguage) {
+    return res.status(400).json({ success: false });
+  }
 
-  // Enregistrez la langue dans la session
   req.session.language = selectedLanguage;
-
+  i18n.setLocale(selectedLanguage);
   res.json({ success: true });
 });
 
 app.listen(port, () => {
-  console.log(`Server start on port : ${port}`);
+  console.log(`Server start on port: ${port}`);
 });
