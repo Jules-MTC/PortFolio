a3793f86a05c8797ea665284e01fef987b7e7818
Jules-MTC <julesantoine11@gmail.com>
2024-05-27 19:11:46 +0200

    chore: Add language switcher functionality

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,29 @@ const multer = require("multer");
 const nodemailer = require("nodemailer");
 const packageJson = require("../package.json");
 const cors = require("cors");
+const i18n = require("i18n");
+const session = require('express-session');
+const secret = require('crypto').randomBytes(64).toString('hex');
 
 const app = express();
 const port = 3000;
-
 const upload = multer();
 
 app.use(cors());
 app.use(upload.none());
+
+i18n.configure({
+  locales: ["en", "fr"],
+  directory: __dirname + '/locales',
+  defaultLocale: "en",
+  objectNotation: true,
+});
+
+app.use(session({
+  secret: secret,
+  resave: false,
+  saveUninitialized: false
+}));
 
 app.post("/send-email", (req, res) => {
   const { name, email, phone, message } = req.body;
@@ -52,6 +67,23 @@ app.post("/send-email", (req, res) => {
 
 app.get("/api/version", (req, res) => {
   res.json({ version: packageJson.version });
+});
+
+// Middleware pour définir la langue à partir de la session
+app.use((req, res, next) => {
+  if (req.session && req.session.language) {
+    i18n.setLocale(req.session.language);
+  }
+  next();
+});
+
+app.post("/api/set-language", (req, res) => {
+  const selectedLanguage = req.body.language;
+
+  // Enregistrez la langue dans la session
+  req.session.language = selectedLanguage;
+
+  res.json({ success: true });
 });
 
 app.listen(port, () => {
